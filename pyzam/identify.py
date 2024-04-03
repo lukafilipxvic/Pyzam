362d4aa46e195fc5f4880b88721ee0bf788d464c
Luka Filipović <lukafilip.vic@gmail.com>
2024-04-04 10:20:19 +1100

    added function comments

--- pyzam/identify.py
+++ pyzam/identify.py
@@ -10,10 +10,19 @@ import os
 
 
 def write_csv(
-    file_name=str, timestamp=str, track_title=str, artist=str, album_cover=str
+    file_name: str, timestamp: str, track_title: str, artist: str, album_cover: str
 ):
     """
-    Writes the song's data into a csv file.
+    Writes the found song's data into a CSV file.
+
+    :param file_name: Name for csv file
+    :param timestamp: Time at which the song was recognized.
+    :param track_title: Track title
+    :param artist: Artist name
+    :param album_cover: URL to album cover image
+
+    Returns:
+        Saves CSV file.
     """
     file_exists = os.path.isfile(f"{file_name}.csv")
 
@@ -24,11 +33,19 @@ def write_csv(
         writer.writerow([timestamp, track_title, artist, album_cover])
 
 
-async def identify_audio(audio_file=str, json=False, write=False, timestamp=None, file_name=None):
+async def identify_audio(
+    audio_file: str, json=False, write=False, timestamp=None, file_name=None
+):
     """
     Identifies the song of a given audio file.
-    Timestamp if write is True.
-    file_name is set to "track_info_{audio_file}" if none is provided. This is used in split_and_identify (mixtape).
+
+    file_name is set to "{audio_file}" if none is provided.
+
+    :param audio_file: Path to audio file.
+    :param json: If true, function returns the whole shazamio output as JSON.
+    :param write: If true, output is written in CSV file. Timestamp is required if write is True.
+    :param timestamp: Time at which the song was recognized. Required for write.
+    :param file_name (Optional): Name for csv file Only valid if write True.
     """
     if not os.path.exists(audio_file):
         print(f"Error: File '{audio_file}' not found.")
@@ -48,32 +65,40 @@ async def identify_audio(audio_file=str, json=False, write=False, timestamp=None
             artist = out["track"]["subtitle"]
             if "images" in out["track"]:
                 album_cover = out["track"]["images"]["coverart"]
+                # Forces the shazam image server to fetch a
+                # high-resolution album cover.
                 album_cover_hq = album_cover.replace(
                     "/400x400cc.jpg", "/1400x1400cc.png"
                 )
-            else:
-                album_cover_hq = None
 
             if write == True:
                 if file_name is not None:
-                    write_csv(f'{file_name}', timestamp, track_title, artist, album_cover_hq)
+                    write_csv(
+                        f"{file_name}", timestamp, track_title, artist, album_cover_hq
+                    )
                 else:
                     file_name = os.path.splitext(os.path.basename(audio_file))[0]
-                    write_csv(f'track_info_{file_name}', timestamp, track_title, artist, album_cover_hq)
+                    write_csv(
+                        f"{file_name}",
+                        timestamp,
+                        track_title,
+                        artist,
+                        album_cover_hq,
+                    )
 
             print(f"Track: {track_title}")
             print(f"Artist: {artist}")
-            print(
-                f"Album Cover: {album_cover_hq}"
-            )
-
+            print(f"Album Cover: {album_cover_hq}")
     else:
         print("No matches found.")
 
 
 def split_and_identify(audio_file, duration=str):
     """
-    Splits a given audio file by the given duration and identifies the songs. Writes the csv file automatically.
+    Splits audio file by duration (seconds) and identifies songs. Writes the csv file automatically.
+
+    :param audio_file: Path to audio file.
+    :param duration: Length of each segment (seconds).
     """
     if not os.path.exists(audio_file):
         print(f"Error: File '{audio_file}' not found.")
@@ -81,6 +106,8 @@ def split_and_identify(audio_file, duration=str):
 
     file_name = os.path.splitext(os.path.basename(audio_file))[0]
 
+    # Reads the audio file and calculates segment count.
+    print(f'Reading {audio_file}...')
     data, samplerate = sf.read(audio_file)
     samples_per_duration = int(samplerate * duration)
     num_segments = len(data) // samples_per_duration
@@ -109,4 +136,5 @@ def split_and_identify(audio_file, duration=str):
                     )
                 )
             progress.update(task, advance=1)
-            time.sleep(2)  #  to slowdown and avoid shazamio call limits.
+            # Shazamio has an call limit, hence we show down the process until we find a fix.
+            time.sleep(2)
--- pyzam/record.py
+++ pyzam/record.py
@@ -2,7 +2,13 @@ import soundcard as sc
 import soundfile as sf
 
 
-def speaker(filename, seconds):
+def speaker(filename: str, seconds):
+    """
+    Records the device's speaker.
+
+    :param filename: Name of audio file written.
+    :param seconds: Duration to record (seconds).
+    """
     with sc.get_microphone(
         id=str(sc.default_speaker().name), include_loopback=True
     ).recorder(samplerate=44100) as speaker:
@@ -10,16 +16,20 @@ def speaker(filename, seconds):
 
         data = speaker.record(numframes=44100 * seconds)
         sf.write(file=filename, data=data, samplerate=44100)
-
     return filename
 
 
-def microphone(filename, seconds):
+def microphone(filename: str, seconds):
+    """
+    Records the device's device.
+    
+    :param filename: Name of audio file written.
+    :param seconds: Duration to record (seconds).
+    """
     with sc.get_microphone(
         id=str(sc.default_microphone().name), include_loopback=True
     ).recorder(samplerate=44100) as mic:
         print(f"Recording microphone for {seconds} seconds...")
         data = mic.record(numframes=44100 * seconds)
         sf.write(file=filename, data=data, samplerate=44100)
-
     return filename
